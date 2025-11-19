6201a008d11de2b4130c3710f2e0a5cb2e1ea6cc
fabs <fabiant941@gmail.com>
2025-11-18 18:19:17 -0800

    feat: Spotify OAuth 2.0 implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,16 @@
-import { APITester } from "./APITester";
 import "./index.css";
-
-import logo from "./logo.svg";
-import reactLogo from "./react.svg";
-
 export function App() {
   return (
-    <div className="max-w-7xl mx-auto p-8 text-center relative z-10">
-      <div className="flex justify-center items-center gap-8 mb-8">
-        <img
-          src={logo}
-          alt="Bun Logo"
-          className="h-24 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa] scale-120"
-        />
-        <img
-          src={reactLogo}
-          alt="React Logo"
-          className="h-24 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa] animate-[spin_20s_linear_infinite]"
-        />
+    <main className="flex flex-col gap-4 items-center justify-center h-screen">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">Spotify Guessing Game</h1>
+        <p className="text-2xl">Guess which friend has this on their playlist</p>
       </div>
 
-      <h1 className="text-5xl font-bold my-4 leading-tight">Bun + React</h1>
-      <p>
-        Edit <code className="bg-[#1a1a1a] px-2 py-1 rounded font-mono">src/App.tsx</code> and save to test HMR
-      </p>
-      <APITester />
-    </div>
+      <a className="bg-[#1ED760] hover:bg-[#1ED760]/80 hover:cursor-pointer text-white font-bold py-2 px-4 rounded" href="/spotify/login">
+        Login with Spotify
+      </a>
+    </main>
   );
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { serve } from "bun";
+import { randomUUIDv5, serve } from "bun";
 import index from "./index.html";
 
 const server = serve({
@@ -6,27 +6,53 @@ const server = serve({
     // Serve index.html for all unmatched routes.
     "/*": index,
 
-    "/api/hello": {
+    "/spotify/login": {
       async GET(req) {
-        return Response.json({
-          message: "Hello, world!",
-          method: "GET",
-        });
-      },
-      async PUT(req) {
-        return Response.json({
-          message: "Hello, world!",
-          method: "PUT",
-        });
+        const state = randomUUIDv5('https://accounts.spotify.com/authorize', 'url');
+        const query = {
+          response_type: 'code',
+          client_id: Bun.env.SPOTIFY_CLIENT_ID!,
+          scope: 'user-read-private user-read-email playlist-read-private playlist-read-collaborative',
+          redirect_uri: 'http://127.0.0.1:3000/spotify/callback',
+          state: state
+        }
+
+        return Response.redirect(`https://accounts.spotify.com/authorize?${new URLSearchParams(query).toString()}`)
       },
     },
 
-    "/api/hello/:name": async req => {
-      const name = req.params.name;
-      return Response.json({
-        message: `Hello, ${name}!`,
-      });
-    },
+    "/spotify/callback": {
+      async GET(req) {
+        const { searchParams } = new URL(req.url);
+        const state = searchParams.get('state');
+        const code = searchParams.get('code');
+        if (!state || !code) {
+          return Response.json({ error: 'No state or code' });
+        }
+
+        const results = await fetch(`https://accounts.spotify.com/api/token`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `Basic ${Buffer.from(`${Bun.env.SPOTIFY_CLIENT_ID}:${Bun.env.SPOTIFY_CLIENT_SECRET}`).toString('base64')}`
+          },
+          body: new URLSearchParams({
+            grant_type: 'authorization_code',
+            code,
+            redirect_uri: 'http://127.0.0.1:3000/spotify/callback',
+            state
+          })
+        })
+
+        if (results.status !== 200) {
+          return Response.json({ error: 'Invalid code' });
+        }
+
+        const data = await results.json()
+        console.log(data)
+        return new Response(data)
+      }
+    }
   },
 
   development: process.env.NODE_ENV !== "production" && {
